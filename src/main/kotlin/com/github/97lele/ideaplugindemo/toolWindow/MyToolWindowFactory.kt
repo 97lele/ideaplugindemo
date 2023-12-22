ae6634b648cbfce078b20f0d81e3ead4773a2087
GitHub Action <action@github.com>
2023-12-22 13:36:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/97lele/ideaplugindemo/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/97lele/ideaplugindemo/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.97lele.ideaplugindemo.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.97lele.ideaplugindemo.MyBundle
+import com.github.97lele.ideaplugindemo.services.MyProjectService
 import javax.swing.JButton
 
 
